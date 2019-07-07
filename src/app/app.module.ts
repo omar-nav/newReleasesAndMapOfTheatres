ba82e371df9d6577d5d988f7f5428b4459992997
omar-nav <omar.navarro@g.austincc.edu>
2019-07-06 20:30:06 -0500

    TODO create component which lists new movies

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TheatresComponent } from './theatres/theatres.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { DataService } from './services/data.service';
+import { TheatresNearbyService } from './services/theatres-nearby.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AppErrorHandler } from './common/validators/app-error-handler';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TheatresComponent
+  ],
+  imports: [
+    BrowserModule,
+    LeafletModule.forRoot(),
+    HttpClientModule
+  ],
+  providers: [
+    DataService,
+    TheatresNearbyService,
+    { provide: ErrorHandler, useClass: AppErrorHandler }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
